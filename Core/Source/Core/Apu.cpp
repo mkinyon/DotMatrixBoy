8f84dcb0fd4edacc4eb134a8ca8372e9a3be785e
mkinyon <mavhuntrx@gmail.com>
2023-12-29 12:38:07 -0700

    Removed SDL dependency from gameboy core

--- Core/Source/Core/Apu.cpp
+++ Core/Source/Core/Apu.cpp
@@ -2,7 +2,6 @@
 #include "Apu.h"
 
 #include <stdint.h>
-#include "../../../ThirdParty/SDL2/include/SDL_stdinc.h"
 
 namespace Core
 {
@@ -26,14 +25,15 @@ namespace Core
 		const double freq = static_cast<double>(440);
 		const double amplitude = 10;
 
-		for (int i = 0; i < len; ++i) {
+		for (int i = 0; i < len; ++i)
+		{
 			// Toggle between two amplitude values
 			double sample = (time < 0.5 / freq) ? amplitude : -amplitude;
 
-			Sint16 intSample = static_cast<Sint16>(sample * 32767.0);  // Convert to signed 16-bit
+			int16_t intSample = static_cast<int16_t>(sample * 32767.0);  // Convert to signed 16-bit
 
-			stream[i] = static_cast<Uint8>(intSample & 0xFF);            // Low byte
-			stream[i + 1] = static_cast<Uint8>((intSample >> 8) & 0xFF);  // High byte
+			stream[i] = static_cast<uint8_t>(intSample & 0xFF);            // Low byte
+			stream[i + 1] = static_cast<uint8_t>((intSample >> 8) & 0xFF);  // High byte
 
 			time += 1.0 / sampleRate;
 			if (time >= 1.0 / freq) {
